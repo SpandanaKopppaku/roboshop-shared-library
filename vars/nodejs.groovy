9dfef85d14c9257acd92f0a72cd214f34c35bb0a
spandanakoppaku <skoppaku@gmail.com>
2023-07-06 11:06:09 -0400

    Auto Push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -15,7 +15,7 @@ pipeline{
         stage("Lint checks"){
           steps{
                 script{
-                   nodejs.lintchecks()
+                   lintchecks()
                 }
             }  
             
