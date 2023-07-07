d903a14a4f04feb0c785535eccccf4830ab8b50e
spandanakoppaku <skoppaku@gmail.com>
2023-07-06 22:51:18 -0400

    Auto Push

---
+++ vars/angularjs.groovy
@@ -0,0 +1,36 @@
+def lintchecks(){
+    sh '''
+        echo Performing lintchecks for ${COMPONENT}
+        echo link checks completed for ${COMPONENT}
+    '''    
+}
+
+def call(COMPONENT){
+pipeline{
+    agent {
+        label 'ws'
+    }
+    stages{                                              //start of the stages
+        stage("Lint checks"){
+          steps{
+                script{
+                   lintchecks()
+                }
+            }  
+            
+        }
+        stage("Code Compile"){
+          steps{
+            //sh "mvn clean compile"
+            }  
+            
+        }
+     }
+   
+   }
+}
+
+
+//          script{
+//                    sample.info('CART')
+//                 }
